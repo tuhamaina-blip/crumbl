6e6690ec66904fbc124f65d0b95da29d252a258b
tuhamaina-blip <tuhamaina@gmail.com>
2026-07-15 11:09:02 +0300

    feat: refactor App component to include Layout and conditional rendering of Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { SavedProvider } from './context/SavedContext';
-import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Layout() {
+  const location = useLocation();
+  const hideLayout = ['/login', '/register'].includes(location.pathname);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-amber-50">
+      {!hideLayout && <Navbar />}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      {!hideLayout && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <SavedProvider>
-          <div className="min-h-screen flex flex-col bg-amber-50">
-            <Navbar />
-            <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/recipes" element={<Recipes />} />
-                <Route path="/login" element={<Login />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <Layout />
         </SavedProvider>
       </AuthProvider>
     </Router>
